c6f6114956488c5d519fe34bd146f8ae84715830
蕭永清 <t112598019@ntut.edu.tw>
2023-10-07 22:57:11 +0800

    1007 hw1 DfsIterator next()

--- src/iterator.cpp
+++ src/iterator.cpp
@@ -59,97 +59,52 @@ void DfsIterator::next(){
     Node * father;
     if(folder){
         father = (*_it)->getParent();
-        cout<< "now folder : " + folder->path() << endl<< endl;
         if(!this->currentItem()->subNodes.empty()){ // have sub nodes;
             _parent = _it;
             _it = this->currentItem()->subNodes.begin();
-            for(auto it = father->subNodes.begin(); it != father->subNodes.end(); it++){
-                cout<< "---------------------" + (*it)->path() << endl;
-            }
-            cout<< "parent folder : " + (* _parent)->path() << endl;
-            cout<< "next folder : " + (*_it)->path() << endl ;
-            cout<< "next name : " + (*_it)->name() << endl << endl;
         }
         else{// don't have sub nodes, so go to next file or folder
             location = std::distance(father->subNodes.begin(), _it);
-            cout<< "parent folder : " + father->path() <<endl;
-            cout<< "location in parent : ";
-            cout<< location << endl<< endl;
-
             if((father->subNodes.begin() + location + 1) != father->subNodes.end()){
                 _it = father->subNodes.begin() + location + 1;
-                cout<< "next file : " + (*_it)->path() << endl ;
-            cout<< "next name : " + (*_it)->name() << endl << endl;
             }
             else{
-                cout<< "end file : " + (*_it)->path() << endl << endl;
                 _it++;
             }
         }
     }
     else{
-        cout<< "now file : " + (*_it)->path() << endl<< endl;
-        if(this->currentItem()->subNodes.begin() != this->currentItem()->subNodes.end()){
-            _it++;
-            cout<< "next file : " + (*_it)->path() << endl ;
-            cout<< "next name : " + (*_it)->name() << endl << endl;
-        }
-        else{
-            father = (*_it)->getParent();
-            grandfather = father->getParent();
-            if(nullptr != grandfather){ // have grandfather
-                if(_it+1 == father->subNodes.end()){
-                    location = std::distance(grandfather->subNodes.begin(), _parent);
-                    
-                    for(auto it = father->subNodes.begin(); it != father->subNodes.end(); it++){
-                        cout<< "---------------------" + (*it)->path() << endl;
-                    }
-
-                    cout<< "grandparent folder : " + grandfather->path() <<endl;
-                    cout<< "parent folder : " + (*_parent)->path() << endl;
-                    cout<< "parent location in grandparent : ";
-                    cout<< location << endl<< endl;
-
-                    if((grandfather->subNodes.begin() + location + 1) != grandfather->subNodes.end()){
-                        _it = grandfather->subNodes.begin() + location + 1;
-                        cout<< "next file : " + (*_it)->path() << endl ;
-                        cout<< "next name : " + (*_it)->name() << endl << endl;
-                    }
-                    else{
-                        cout<< "end file : " + (*_it)->path() << endl << endl;
-                        _it = grandfather->subNodes.end();
-                    }
-                    
-                    if(nullptr != grandfather->getParent()){
-                        for(auto it = grandfather->getParent()->subNodes.begin(); it != grandfather->getParent()->subNodes.end(); it++){
-                            if((*it)->path() == grandfather->path()){
-                                _parent = it;
-                            }
+        father = (*_it)->getParent();
+        grandfather = father->getParent();
+        if(nullptr != grandfather){ // have grandfather
+            if(_it+1 == father->subNodes.end()){
+                location = std::distance(grandfather->subNodes.begin(), _parent);
+                if((grandfather->subNodes.begin() + location + 1) != grandfather->subNodes.end()){
+                    _it = grandfather->subNodes.begin() + location + 1;
+                }
+                else{
+                    _it = grandfather->subNodes.end();
+                }
+                if(nullptr != grandfather->getParent()){
+                    for(auto it = grandfather->getParent()->subNodes.begin(); it != grandfather->getParent()->subNodes.end(); it++){
+                        if((*it)->path() == grandfather->path()){
+                            _parent = it;
                         }
                     }
                 }
-                else{
-                    _it++;
-                    cout<< "next file : " + (*_it)->path() << endl ;
-                    cout<< "next name : " + (*_it)->name() << endl << endl;
-                }
             }
-            else{ // only have father
-                location = std::distance(father->subNodes.begin(), _it);
-                cout<< "parent folder : " + father->path() <<endl;
-                cout<< "location in parent : ";
-                cout<< location << endl<< endl;
-
-                if((father->subNodes.begin() + location + 1) != father->subNodes.end()){
-                    _it = father->subNodes.begin() + location + 1;
-                    cout<< "next file : " + (*_it)->path() << endl ;
-                    cout<< "next name : " + (*_it)->name() << endl << endl;
-                }
-                else{
-                    cout<< "end file : " + (*_it)->path() << endl << endl;
-                    _it++;
-                    
-                }
+            else{
+                _it++;
+            }
+        }
+        else{ // only have father
+            location = std::distance(father->subNodes.begin(), _it);
+            if((father->subNodes.begin() + location + 1) != father->subNodes.end()){
+                _it = father->subNodes.begin() + location + 1;
+            }
+            else{
+                _it++;
+                
             }
         }
     }
