69fddf90ff7d6236061de53f2175f883c6f0e279
蕭永清 <t112598019@ntut.edu.tw>
2023-10-12 22:14:24 +0800

    1012 hw2 getPaths

--- src/find_by_name_visitor.h
+++ src/find_by_name_visitor.h
@@ -7,14 +7,21 @@
 class FindByNameVisitor : public Visitor{
 private:
     string _name;
+    list<string> paths;
 public:
     FindByNameVisitor(string name)
     : _name(name)
     {}
 
     void visitFolder(Folder * folder){
-        
+
     }
 
     void visitFile(File * file){}
+
+    list<string> getPaths(){
+        for(auto it = this->_nodes.begin(); it != this->_nodes.end(); it++){
+            paths.push_back(*it);
+        }
+    }
 };
