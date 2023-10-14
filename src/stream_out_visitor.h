c427c1948e13dd1b02440eb45e7af647bed60b0b
蕭永清 <t112598019@ntut.edu.tw>
2023-10-14 13:09:35 +0800

    1014 hw2 update given code : findByName

--- src/stream_out_visitor.h
+++ src/stream_out_visitor.h
@@ -5,10 +5,16 @@
 #include "./file.h"
 
 class StreamOutVisitor : public Visitor{
+private:
+    list<string> _result;
 public:
     StreamOutVisitor(){}
 
     void visitFolder(Folder * folder){}
 
     void visitFile(File * file){}
+
+    list<string> getResult(){
+        return _result:
+    }
 };
